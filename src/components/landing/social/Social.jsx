daecb52843287bea715c793bf383c7558f861fe6
Ivan-San <ivohan8@gmail.com>
2020-10-31 17:27:55 +0100

    Social component updated

--- src/components/landing/social/Social.jsx
+++ src/components/landing/social/Social.jsx
@@ -8,26 +8,29 @@ function Social() {
     const videoWidth = 200;
     const videoHeight = 164.29;
 
-
-
     return (
             <div className="landing__container__social">
                 {
                     videos.map((video) => {
                         if (video.type === youtubeVideo) {
                             return (
-                                <div className="landing__container__social__video social__block-element" id={video.title}>
-                                    <iframe width={videoWidth} height={videoHeight} src={video.url} 
-                                    frameborder="0"  title={video.title} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                                    allowfullscreen>
-                                    </iframe>
+                                <div class="landing__container__social__streaming-block">
+                                    <span class="landing__container__social__video__title">{video.title}</span>
+                                    <div className="landing__container__social__video social__block-element" id={video.title}>
+                                        <iframe width={videoWidth} height={videoHeight} src={video.url} 
+                                        frameborder="0"  title={video.title} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen">
+                                        </iframe>
+                                    </div>
                                 </div>
                                 
                             )
                         } else if (video.type === streamingVideo) {
                             return (
-                                <div className="landing__container__social__streaming social__block-element" style={{backgroundImage: `url(${video.image})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-                                    <img src={video.icon} alt={video.title} class="landing__container__social__streaming__twitch-icon"/>
+                                <div class="landing__container__social__streaming-block">
+                                    <span class="landing__container__social__video__title">{video.title}</span>
+                                    <div className="landing__container__social__streaming social__block-element" style={{backgroundImage: `url(${video.image})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+                                        <img src={video.icon} alt={video.title} class="landing__container__social__streaming__twitch-icon"/>
+                                    </div>
                                 </div>
                             )
                         }
