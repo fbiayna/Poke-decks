81afe4e183a40d566939ff4097847f42fa423f08
Ivan-San <ivohan8@gmail.com>
2020-10-31 11:12:11 +0100

    videos file added

---
+++ src/components/social/video/videos.js
@@ -0,0 +1,9 @@
+const youtubeVideos = [
+    {title: 'tutorial', url: 'https://www.youtube.com/watch?v=FDqDvvJyY2w', originalName: 'How to Play Pokémon TCG Tutorial', type: 'youtube-video'},
+    {title: 'gameplay', url: 'https://www.youtube.com/watch?v=V0LKOyqOWLU', originalName: 'Pokémon TCG Online [Sword & Shield: Cinderace Theme Deck] (No Commentary)', type: 'youtube-video'},
+    {title: 'championship', url: 'https://www.youtube.com/watch?v=pp7up3LK2gk', originalName: '2019 Pokémon Europe International Championships: Pokémon TCG Masters Division Finals', type: 'youtube-video'},
+    {title: 'Twitch Official', url: 'https://www.twitch.tv/pokemontcg', originalName: 'Twitch - Pokemon TCG', type: 'streaming', image: 'https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/20108c30241c87521068_pokemontcg_36987808816_1380506236/thumb/thumb0-320x180.jpg',},
+    {title: 'championship', url: 'https://www.twitch.tv/videos/555711636', originalName: '2020 Pokémon Oceania International Championships - TCG Day 2', type: 'streaming', image: 'https://trello-attachments.s3.amazonaws.com/5f8ca3639574d3550b3ad495/5f9cb0e6b6fed24123256da4/8248430e184b0dfd8a62e58ab7d6fec4/555711636-strip-0_(1).jpg', icon: 'https://assets.stickpng.com/images/580b57fcd9996e24bc43c540.png',}
+]
+
+export default youtubeVideos;
