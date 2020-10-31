171f94e108ce62a3a396851660818d83241f7fe6
Ivan-San <ivohan8@gmail.com>
2020-10-31 18:54:49 +0100

    Landing file fixed

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
+import Social from './Social/Social';
+import FirstRow from './First-Row/FirstRow';
 
 function Landing() {
-	return (
-		<div className="landing__header--first-row">
-			<div className="landing__header--bg-image"></div>
-			<div className="landing__header--content">
-				<p className="landing__header--description">
-					“ Deck-Poke Masters is a database built around trading card game of
-					Pokemon. You will be able to consult from all Pokemon’s TCG sets to
-					every existing card in the game. In addition, we offer a competitive
-					tool such a deck editor, where it will provide you detailed statistics
-					to become a master Pokemon trainer. ”
-				</p>
-				<div className="landing__header--logo">
-					<img src="" alt="website-logo" />
-				</div>
-			</div>
-		</div>
-	);
+
+    return <>
+        <FirstRow />
+        <Social />
+    </>
 }
 
 export default Landing;
