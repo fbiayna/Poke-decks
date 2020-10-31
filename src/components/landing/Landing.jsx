7effa36c8bde848ca1345ca61a153c3966ae3e16
Ivan-San <ivohan8@gmail.com>
2020-10-31 11:43:14 +0100

    Landing component created

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Social from './social/Social';
+
+
+function Landing() {
+
+    return <div className="landing">
+        <Social />
+    </div>
+}
+
+export default Landing;
