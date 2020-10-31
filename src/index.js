e45b74ee04a89942d31ef41c16592a0a33bc6855
Ivan-San <ivohan8@gmail.com>
2020-10-31 17:33:29 +0100

    Moved react-router-dom from Landing.jsx to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Landing from './components/Landing/Landing';
