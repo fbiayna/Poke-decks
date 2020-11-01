2fd4553927b6a5f44324059c7bdc0d603aac3859
fbiayna <ferranbiaynacosta@gmail.com>
2020-11-01 16:33:18 +0100

    Cards-Section Landing -Done

--- src/components/Landing/Cards-Section/Cards-Search/CardSearchLanding.jsx
+++ src/components/Landing/Cards-Section/Cards-Search/CardSearchLanding.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import cardsStore from '../../../stores/store';
-import { loadRandomCards } from '../../../actions/action-creators'
-import './CardSection.css';
+import cardsStore from '../../../../stores/store';
+import { loadRandomCards } from '../../../../actions/action-creators'
+import './CardSearchLanding.css';
 
-function CardSection() {
+function CardSearchLanding() {
 
     const [cards, setCards] = useState([]);
 
@@ -27,12 +27,12 @@ function CardSection() {
     return (
         <div className="landing__container__cardSection">
             <div className="cardSection__images">
-                <Link to="/cards"><div className="images__title">
+                <Link className="title__link" to="/cards"><div className="images__title">
                     <h2>SEARCH BY CARDS</h2>
                     <p>Do you need information about any card?<br></br>Find it with Master Deck' search tool!</p>
                 </div></Link>
                 <div className="images__cards">
-                {cards?.map((card,index) => <Link to="/cards"><img className="cards__png-info" id={`cards__png-info-${index}`} alt='error' src={card.imageUrl}/></Link>)}
+                {cards?.map((card,index) => <Link to={`/detail/${card.id}`}><img className="cards__png-info" id={`cards__png-info-${index}`} alt='error' src={card.imageUrl}/></Link>)}
                 </div>
                 <div className="images__button-search">
                     <Link to="/cards"><button id="button-search__cards-home">Find Cards</button></Link>
@@ -43,4 +43,4 @@ function CardSection() {
 
 }
 
-export default CardSection
+export default CardSearchLanding
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Social from './Social/Social';
 import FirstRow from './First-Row/FirstRow';
-import CardSection from './Cards-Section/CardSection';
+import CardSearchLanding from './Cards-Section/Cards-Search/CardSearchLanding';
 
 function Landing() {
 
     return <>
         <FirstRow />
-        <CardSection/>
+        <CardSearchLanding/>
         <Social />
     </>
 }
