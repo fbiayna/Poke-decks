d58fa17d9820641c6ea02fa077005b20e1876fcb
Alberto <algmebe1@gmail.com>
2020-11-01 19:51:49 +0100

    FirstRow with responsive

--- src/components/Landing/First-Row/FirstRow.jsx
+++ src/components/Landing/First-Row/FirstRow.jsx
@@ -7,11 +7,11 @@ function FirstRow() {
 			<div className="landing__header--bg-image"></div>
 			<div className="landing__header--content">
 				<p className="landing__header--description">
-					“ Deck-Poke Masters is a database built around trading card game of
-					Pokemon. You will be able to consult from all Pokemon’s TCG sets to
-					every existing card in the game. In addition, we offer a competitive
-					tool such a deck editor, where it will provide you detailed statistics
-					to become a master Pokemon trainer. ”
+					“ Poke-Decks is a database built around trading card game of Pokemon.
+					You will be able to consult from all Pokemon’s TCG sets to every
+					existing card in the game. In addition, we offer a competitive tool
+					such a deck editor, where it will provide you detailed statistics to
+					become a master Pokemon trainer. ”
 				</p>
 				<div className="landing__header--logo">
 					<img
