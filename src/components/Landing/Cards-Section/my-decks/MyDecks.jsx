7ea2c58c66f3f97c0dc346c564d9ed87005edcd0
Ivan-San <ivohan8@gmail.com>
2020-11-01 15:57:23 +0100

    MyDecks.jsx structure improved

--- src/components/Landing/Cards-Section/my-decks/MyDecks.jsx
+++ src/components/Landing/Cards-Section/my-decks/MyDecks.jsx
@@ -10,21 +10,27 @@ function MyDecks() {
         totalcards: 0, 
         id:0,
         imagealt: 'card-pile',
-        url: 'https://trello-attachments.s3.amazonaws.com/5f8ca3639574d3550b3ad495/5f9cb0e6b6fed24123256da4/0b8025e8ab38f7b89454be9b91dd114a/91yL1H9fBdL.svg'}
+        url: 'https://trello-attachments.s3.amazonaws.com/5f8ca3639574d3550b3ad495/5f9cb0e6b6fed24123256da4/0b8025e8ab38f7b89454be9b91dd114a/91yL1H9fBdL.svg'
+    }
     ];
 
-    return (
-            <div className="cardsSection__myDeck">
-                {
-                    myDecks.map((deck) => 
-                    <Link to="/my-decks" className="MyDeck-Links">
-                        <div className="cardsSection__myDeck__deckBlock">
-                            <span className="cardsSection__myDeck__deckBlock__title">{deck.title}</span>
-                            <img src={deck.url} alt={deck.imagealt}/>
-                        </div>
-                    </Link>
-                    )
-                }
+    return (    
+            <div className="cardsSection__myDeck__block">
+                <div className="cardsSection__myDeck__titleBlock">
+                <span className="cardsSection__myDeck__title" id="title">MY DECKS</span>
+                </div>
+                <div className="cardsSection__myDeck">
+                    {
+                        myDecks.map((deck) => 
+                        <Link to="/my-decks" className="MyDeck-Links">
+                            <div className="cardsSection__myDeck__deckBlock">
+                                <span className="cardsSection__myDeck__deckBlock__title">{deck.title}</span>
+                                <img src={deck.url} alt={deck.imagealt}/>
+                            </div>
+                        </Link>
+                        )
+                    }
+                </div>
             </div>
     );
 }
