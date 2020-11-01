76e0f8251700a82a882ec093583df927658e223b
Ivan-San <ivohan8@gmail.com>
2020-11-01 12:44:28 +0100

    added CardsSection component into Landing.jsx

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Social from './Social/Social';
 import FirstRow from './First-Row/FirstRow';
+import CardsSection from './Cards-Section/CardsSection';
 
 function Landing() {
 
     return <>
         <FirstRow />
+        <CardsSection />
         <Social />
     </>
 }
