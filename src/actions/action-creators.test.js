1794008f0b84c9f5bcc095cc6ce4ba1962fb1306
Ivansannicolas <ivohan8@gmail.com>
2020-11-02 23:07:15 +0100

    loadCards call-dispatch and call-dispatch-just-once written

--- src/actions/action-creators.test.js
+++ src/actions/action-creators.test.js
@@ -8,7 +8,17 @@ jest.mock('../dispatcher/dispatcher');
 describe('action-creators', () => {
     describe('loadCards', () => {
         beforeEach(async () => {
-            axios.mockImplementationOnce(() => {Promise.resolve({ data: [] })})
+            axios.mockImplementationOnce(() => { Promise.resolve({ data: [] }) });
+            await loadCards();
+        });
+
+        test('should call dispatch', () => {
+            expect(dispatcher.dispatch.mock.calls[0][0]).toEqual({ type: 'load_cards', payload: [] });
+        });
+        
+        test('shold call dispatch just once', () => {
+            expect(dispatcher.dispatch.mock.calls.length).toBe(1);
         })
-    })
+
+    });
 })
