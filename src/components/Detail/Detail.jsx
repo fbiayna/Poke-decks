1dc34f5704b03008680e33390533238c40cffb4d
fbiayna <ferranbiaynacosta@gmail.com>
2020-11-02 16:54:12 +0100

    Detail -fix return functions

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 import cardsStore from '../../stores/store';
-import './Detail.css'
+import './Detail.css';
 import { loadCard } from '../../actions/action-creators';
 
 function Detail(props) {
-	debugger
+	debugger;
 	const [cardId] = useState(props.match.params.cardid);
 	const [card, setCard] = useState(null);
 
@@ -22,56 +23,124 @@ function Detail(props) {
 		return () => {
 			cardsStore.removeEventListener(handleChange);
 		};
-
 	}, [card, cardId]);
 
 	function rules() {
 		if (Array.isArray(card?.card.text)) {
-			return `Rules: ${card.card.text}`
+			return `Rules: ${card.card.text}`;
 		}
 	}
 
 	function pokedex() {
-		if (card?.card.nationalPokedexNumber!==undefined) {
-			return `Pokédex Number: #${card?.card.nationalPokedexNumber}`
+		if (card?.card.nationalPokedexNumber !== undefined) {
+			return `Pokédex Number: #${card?.card.nationalPokedexNumber}`;
 		}
 	}
 
 	function pokemonHp() {
-		if (card?.card.hp!==undefined) {
-			return `HP: ${card?.card.hp}`
+		if (card?.card.hp !== undefined) {
+			return `HP: ${card?.card.hp}`;
+		}
+	}
+
+	function pokemonEvolve() {
+		if (card?.card.evolvesFrom !== undefined) {
+			return (
+				<div>
+					<span>Evolve from: </span>
+					<Link to={`./cards/?name=${card?.card.evolvesFrom}`}><span>{card?.card.evolvesFrom}</span></Link>
+				</div>
+			)
 		}
 	}
 
 	function pokemonType() {
-		if (card?.card.types!==undefined) {
-			return card.card.types.map((type) => <i className={`energy ${type}`}></i>)
+		if (card?.card.types !== undefined) {
+			return card.card.types.map((type) => (
+				<i className={`energy ${type}`}></i>
+			));
 		}
 	}
 
 	function pokemonAttacks() {
-		if (card?.card.attacks!==undefined) {
-		return card.card.attacks.map((attack) => <li>{attack.cost.map((energy) => <i className={`energy ${energy}`}></i>)}<span>{attack.name} | {attack.damage}</span><p>{attack.text}</p><p>Converted Energy Cost: {attack.convertedEnergyCost}</p></li>)
+		if (card?.card.attacks !== undefined) {
+			return card.card.attacks.map((attack) => (
+				<li>
+					{attack.cost.map((energy) => (
+						<i className={`energy ${energy}`}></i>
+					))}
+					<span>
+						{attack.name} | {attack.damage}
+					</span>
+					<p>{attack.text}</p>
+					<p>Converted Energy Cost: {attack.convertedEnergyCost}</p>
+				</li>
+			));
 		}
 	}
 
 	function pokemonResist() {
-		if (card?.card.resistances!==undefined) {
-		return card.card.resistances.map((resist) => <li><i className={`energy ${resist.type}`}></i><span>{resist.value}</span></li>)
+		if (card?.card.resistances !== undefined) {
+			return (
+				<div className="pokemon__resistance">
+					<span>Resistances: </span>
+					<span>
+						{card.card.resistance.map((resist) => (
+							<li>
+								<i className={`energy ${resist.type}`}></i>
+								<span>{resist.value}</span>
+							</li>
+						))}
+					</span>
+				</div>
+			);
 		}
 	}
 
 	function pokemonWeak() {
-		if (card?.card.weaknesses!==undefined) {
-		return card.card.weaknesses.map((weak) => <li><i className={`energy ${weak.type}`}></i><span>{weak.value}</span></li>)
-			}
+		if (card?.card.weaknesses !== undefined) {
+			return (
+				<div className="pokemon__weakness">
+					<span>Weakness: </span>
+					<span>
+						{card.card.weaknesses.map((weak) => (
+							<li>
+								<i className={`energy ${weak.type}`}></i>
+								<span>{weak.value}</span>
+							</li>
+						))}
+					</span>
+				</div>
+			);
+		}
+	}
+
+	function pokemonRetreat() {
+		if (card?.card.retreatCost !== undefined) {
+			return (
+				<div className="pokemon__retreat">
+					<span>Retreat Cost: </span>
+					<span>
+						{card.card.retreatCost.map((retreat) => (
+							<li>
+								<i className={`energy ${retreat}`}></i>
+							</li>
+						))}
+					</span>
+				</div>
+			);
+		}
 	}
 
 	return (
 		<>
 			<div className="detail-container">
 				<div className="detail-container__image">
-					<img id="image__poke-card" alt="error" src={card?.card.imageUrlHiRes}></img>
+					<img
+						id="image__poke-card"
+						alt="error"
+						src={card?.card.imageUrlHiRes}
+					></img>
 				</div>
 				<div className="detail-container__description">
 					<div className="description__title">
@@ -89,10 +158,12 @@ function Detail(props) {
 					<div className="description__pokemon">
 						<p>{pokedex()}</p>
 						<p>{pokemonHp()}</p>
+						<p>{pokemonEvolve()}</p>
 						<ul>{pokemonType()}</ul>
 						<ul>{pokemonAttacks()}</ul>
 						<ul>{pokemonResist()}</ul>
 						<ul>{pokemonWeak()}</ul>
+						<ul>{pokemonRetreat()}</ul>
 					</div>
 				</div>
 			</div>
