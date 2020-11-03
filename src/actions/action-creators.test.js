4bd1b7bc36d1b219115668f2663636215daf120a
Ivansannicolas <ivohan8@gmail.com>
2020-11-03 10:22:30 +0100

    added should call axios with API test

--- src/actions/action-creators.test.js
+++ src/actions/action-creators.test.js
@@ -1,6 +1,7 @@
-import axios from 'axio';
+import axios from 'axios';
 import dispatcher from '../dispatcher/dispatcher';
 import { loadCards } from '../actions/action-creators';
+import actionTypes from './actiontypes';
 
 jest.mock('axios');
 jest.mock('../dispatcher/dispatcher');
@@ -8,16 +9,26 @@ jest.mock('../dispatcher/dispatcher');
 describe('action-creators', () => {
     describe('loadCards', () => {
         beforeEach(async () => {
-            axios.mockImplementationOnce(() => { Promise.resolve({ data: [] }) });
+            axios.mockImplementationOnce(() =>  Promise.resolve({ data: [] }));
             await loadCards();
         });
 
         test('should call dispatch', () => {
-            expect(dispatcher.dispatch.mock.calls[0][0]).toEqual({ type: 'load_cards', payload: [] });
+          expect(dispatcher.dispatch.mock.calls[0][0]).toEqual({ type: actionTypes.loadCards, payload: [] });
         });
         
         test('shold call dispatch just once', () => {
             expect(dispatcher.dispatch.mock.calls.length).toBe(1);
+        });
+
+        test('should call axios with cards API', async () => {
+            // arrange
+            const idpage = 1;
+            const sizepage = 500;
+            const response = await fetch(`https://api.pokemontcg.io/v1/cards?page=${idpage}&pageSize=${sizepage}`);
+            const cards = await response.json();
+            // assert
+            expect(axios.mock.calls[0][0]).toEqual(cards);
         })
 
     });
