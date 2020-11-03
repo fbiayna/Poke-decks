be35c92683339989031fda058b9864dbe9586cdf
Ivansannicolas <ivohan8@gmail.com>
2020-11-03 11:12:22 +0100

    loadCards test finished for now, loadRandomCards tests started

--- src/actions/action-creators.test.js
+++ src/actions/action-creators.test.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import dispatcher from '../dispatcher/dispatcher';
-import { loadCards } from '../actions/action-creators';
+import { loadCards, loadRandomCards } from '../actions/action-creators';
 import actionTypes from './actiontypes';
 
 jest.mock('axios');
@@ -9,12 +9,12 @@ jest.mock('../dispatcher/dispatcher');
 describe('action-creators', () => {
     describe('loadCards', () => {
         beforeEach(async () => {
-            axios.mockImplementationOnce(() =>  Promise.resolve({ data: [] }));
+            axios.mockImplementationOnce(() => Promise.resolve({ data: [] }));
             await loadCards();
         });
 
         test('should call dispatch', () => {
-          expect(dispatcher.dispatch.mock.calls[0][0]).toEqual({ type: actionTypes.loadCards, payload: [] });
+            expect(dispatcher.dispatch.mock.calls[0][0]).toEqual({ type: actionTypes.loadCards, payload: [] });
         });
         
         test('shold call dispatch just once', () => {
@@ -22,14 +22,23 @@ describe('action-creators', () => {
         });
 
         test('should call axios with cards API', async () => {
-            // arrange
-            const idpage = 1;
-            const sizepage = 500;
-            const response = await fetch(`https://api.pokemontcg.io/v1/cards?page=${idpage}&pageSize=${sizepage}`);
-            const cards = await response.json();
             // assert
-            expect(axios.mock.calls[0][0]).toEqual(cards);
-        })
-
+            expect(axios.mock.calls[0][0]).toEqual('https://api.pokemontcg.io/v1/cards?page=1&pageSize=500');
+        });
+        
+        test('should call axios just once', () => {
+            expect(axios.mock.calls.length).toBe(1);
+        });
     });
-})
+
+    describe('loadRandomCards', () => {
+        beforeEach(async () => {
+            axios.mockImplementationOnce(() => Promise.resolve([]));
+            await loadRandomCards();
+        });
+        test('should call dispatch', () => {
+            expect(dispatcher.dispatch.mock.calls[0][0]).toEqual({ type: actionTypes.loadRandomCards, payload: [] });
+        });
+        
+    })
+});
