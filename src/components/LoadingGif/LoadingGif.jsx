f3348a3cfbe8297f01d7d2c1da1f0c6dd7893015
Ivansannicolas <ivohan8@gmail.com>
2020-11-04 16:02:42 +0100

    updated gif url

--- src/components/LoadingGif/LoadingGif.jsx
+++ src/components/LoadingGif/LoadingGif.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './LoadingGif.css';
-import 'simple_pokeball.gif';
 
 function LoadingGif() {
     return (
         <div className="loading-gif">
-            <img src="simple_pokemon.gif" alt="loading-gif" className="loading-gif__image"/>
+            <img src="https://trello-attachments.s3.amazonaws.com/5f8ca3639574d3550b3ad495/5f9cb0e6b6fed24123256da4/15c07b009de08e498ed1d40a3ef380de/simple_pokeball.gif" alt="loading-gif" className="loading-gif__image"/>
         </div>
     );
 }
