7687560b55ad50f6df84002231f869a37484a286
Ivansannicolas <ivohan8@gmail.com>
2020-11-05 21:31:12 +0100

    Deck.jsx divided into two components

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -1,37 +1,16 @@
-import React, { useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
-import cardsStore from '../../stores/store';
-import { loadDecks } from '../../'
+import React from 'react';
+import DeckDetails from './DeckDetails/DeckDetails';
+import DeckCards from './DeckCards/DeckCards';
 
-function Decks {
-    const [decks, setDecks] = useState(cardsStore.getDecks());
+function Decks() {
 
-    function handleChange() {
-        setDecks(cardsStore.getDecks());
-    }
-
-    useEffect(() => {
-        cardsStore.addEventListener(handleChange);
-
-        if (!decks.length) {
-            loadDecks();
-        }
-
-        return () => { cardsStore.removeEventListener(handleChange) }
-    }, [decks]);
     
     return (
         <div className="decks__section">
-            <div className="decks__section__block deck__details">
-                <div className="decks__section__block__deck-description"></div>
-                <div className="decks__section__block__deck-statistics"></div>
-            </div>
-            <div className="decks__section__block deck__add-cards">
-                <div className="decks__section__block deck__add-cards__add-button"></div>
-                <div className="decks__section__block deck__add-cards__total-cards"></div>
-            </div>
-            <div className="decks__section__block deck__cards">
-            </div>
+            < DeckDetails />
+            <DeckCards />
         </div>
     )
 }
+
+export default Decks;
