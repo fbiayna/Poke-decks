6a44599cbb6d47f2751ecfc364bfa14b246cd70b
Ivansannicolas <ivohan8@gmail.com>
2020-11-05 22:00:49 +0100

    html and style added to DeckDetails component

--- src/components/Deck/DeckDetails/DeckDetails.jsx
+++ src/components/Deck/DeckDetails/DeckDetails.jsx
@@ -21,15 +21,32 @@ function DeckDetails() {
         return () => { cardsStore.removeEventListener(handleChange) }
     }, [decks]);
 
+    function onChange(setValue, event) {
+        setValue(event);
+    }
+
     return (
         <>
             <div className="decks__section__block deck__details">
-                <div className="decks__section__block__deck-description"></div>
-                <div className="decks__section__block__deck-statistics"></div>
+                <div className="decks__section__block__deck-description">
+                    <input type="text" name="deck-title" id="name-title" value={decks[0].title} onChange={(event)=>{onChange(loadDecks, event.target.value)}}/>
+                    <input type="text" name="deck-description" id="name-description" value={decks[0].description} onChange={(event)=>{onChange(loadDecks, event.target.value)}}/>
+                </div>
+                <div className="decks__section__block__deck-statistics">
+                    <span>TEXTOOOOOOOO</span>
+                    <span>TEXTOOOOOOOO</span>
+                    <span>TEXTOOOOOOOO</span>
+                    <span>TEXTOOOOOOOO</span>
+                    <span>TEXTOOOOOOOO</span>
+                </div>
             </div>
             <div className="decks__section__block deck__add-cards">
-                <div className="decks__section__block deck__add-cards__add-button"></div>
-                <div className="decks__section__block deck__add-cards__total-cards"></div>
+                <div className="decks__section__block__deck__add-cards__add-button">
+                    <span>ADD CARDS</span>
+                </div>
+                <div className="decks__section__block__deck__add-cards__total-cards">
+                    <span>0/60 CARDS</span>
+                </div>
             </div>
         </>    
 
