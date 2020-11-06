eff7b0519e8c693a98fa029ed2ad9631b22e87d9
Ivansannicolas <ivohan8@gmail.com>
2020-11-06 04:22:16 +0100

    Deck component updated with styles

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import DeckDetails from './DeckDetails/DeckDetails';
 import DeckCards from './DeckCards/DeckCards';
+import './Deck.css'
 
 function Decks() {
 
-    
     return (
-        <div className="decks__section">
-            < DeckDetails />
+        <div className="deck">
+            <DeckDetails />
             <DeckCards />
         </div>
     )
