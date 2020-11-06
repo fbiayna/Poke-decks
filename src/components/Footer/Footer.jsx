394193a2bd864d3c56c1fbb7d1cff2e3accd8153
Alberto <algmebe1@gmail.com>
2020-11-06 21:48:28 +0100

    footer replaced

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,73 +4,65 @@ import './Footer.css';
 function Footer() {
 	return (
 		<div className="footer-container">
+			<div className="footer__follow">
+				<h5>FOLLOW US</h5>
+				<ul>
+					<li>
+						<a href="#">
+							<i class="fa fa-facebook-f"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-twitter"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-google-plus"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-instagram"></i>
+						</a>
+					</li>
+					<li>
+						<a href="https://github.com/SkylabCoders/team-rocket">
+							<i class="fa fa-github"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
 			<div className="footer__about-us">
 				<h5>ABOUT US</h5>
-				<p>You can reach us at: info@poke-decks.com</p>
 
 				<div className="footer__info">
 					<div className="address">
-						<h6 className="footer__about-us--headers">ADDRESS</h6>
+						<h6 className="footer__about-us--headers">
+							<i class="fa fa-map-marker"></i>ADDRESS
+						</h6>
 						<address>Pallet Town 35 - 08005 Kanto</address>
 					</div>
 
 					<br />
 
 					<div className="email">
-						<h6 className="footer__about-us--headers">HAVE ANY QUESTIONS?</h6>
+						<h6 className="footer__about-us--headers">
+							<i class="fa fa-envelope"></i>HAVE ANY QUESTIONS?
+						</h6>
 						<p>info@poke-decks.com</p>
 					</div>
 
 					<br />
 
 					<div className="phone">
-						<h6 className="footer__about-us--headers">PHONE</h6>
+						<h6 className="footer__about-us--headers">
+							<i class="fa fa-phone"></i>PHONE
+						</h6>
 						<p>+34 (6) 40 12-9873-12345</p>
 					</div>
 				</div>
-			</div>
-
-			<div className="footer__follow">
-				<h5>FOLLOW US</h5>
-				<ul>
-					<li>
-						<img
-							src="https://www.flaticon.es/svg/static/icons/svg/733/733549.svg"
-							alt="fb-logo"
-							className="footer__follow__img"
-						/>
-					</li>
-					<li>
-						<img
-							src="https://www.flaticon.es/svg/static/icons/svg/733/733579.svg"
-							alt="twitter-logo"
-							className="footer__follow__img"
-						/>
-					</li>
-					<li>
-						<img
-							src="https://www.flaticon.es/svg/static/icons/svg/733/733557.svg"
-							alt="gplus-logo"
-							className="footer__follow__img"
-						/>
-					</li>
-					<li>
-						<img
-							src="https://www.flaticon.es/svg/static/icons/svg/174/174855.svg"
-							alt="instagram-logo"
-							className="footer__follow__img"
-						/>
-					</li>
-					<li>
-						<a href="https://github.com/SkylabCoders/team-rocket">
-							<img
-								src="https://www.flaticon.es/svg/static/icons/svg/733/733609.svg"
-								alt="github-logo"
-								className="footer__follow__img"
-							/>
-						</a>
-					</li>
-				</ul>
 			</div>
 		</div>
 	);
