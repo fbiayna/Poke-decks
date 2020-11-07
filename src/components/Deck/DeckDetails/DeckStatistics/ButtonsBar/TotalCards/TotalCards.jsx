6ba1c947376b819b1be61831a0fb77f362607eea
Ivansannicolas <ivohan8@gmail.com>
2020-11-07 16:21:03 +0100

    added icon to TotalCards

--- src/components/Deck/DeckDetails/DeckStatistics/ButtonsBar/TotalCards/TotalCards.jsx
+++ src/components/Deck/DeckDetails/DeckStatistics/ButtonsBar/TotalCards/TotalCards.jsx
@@ -4,8 +4,15 @@ function TotalCards(cards) {
     debugger;
     return (
         <div className="decks__section__block__deck__add-cards__total-cards">
-            {cards ? <span id="length-true">{cards.cards?.length}/60 CARDS</span> : <span id="length-false" >0/60 CARDS</span>}
+            {cards ?
+                <div className="totalCards-button">
+                    <span id="length-true">{cards.cards?.length}/60 CARDS</span>
+                    <div className="total-cards__pokedex"/>
+                </div>
+                :
+                <span id="length-false" >0/60 CARDS</span>}
         </div>
+        
     );
 }
 
