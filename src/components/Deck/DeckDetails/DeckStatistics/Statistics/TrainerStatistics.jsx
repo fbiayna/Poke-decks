658035ec69679d2bd91bd2c5df3e6f8c182d65fa
Ivansannicolas <ivohan8@gmail.com>
2020-11-07 15:42:07 +0100

    added title to trainer satistic

--- src/components/Deck/DeckDetails/DeckStatistics/Statistics/TrainerStatistics.jsx
+++ src/components/Deck/DeckDetails/DeckStatistics/Statistics/TrainerStatistics.jsx
@@ -4,31 +4,35 @@ function TrainerStatistics({ decks }) {
 	let totalTrainers = decks[0]?.cards?.filter((card) => card.supertype === 'Trainer').length;
 
 	return (
-		<>
-			{totalTrainers < 20 ? (
-				<div className="statistics__row">
-					<div className="stateIcon warningIcon"></div>
-					<span id="stat-warning" className="statistic warning">
-						You have {totalTrainers} trainers and we recommend to have at least 20
-					</span>
+			<div className="statistics__category" id="statistic__category__trainers">
+				<div className="statistics__category__name">
+					<h2 className="statistics__category-title">Trainers</h2>
+					<div className="statistics__category-icon"></div>
 				</div>
-			) : null}
-			{totalTrainers > 38 ? (
-				<div className="statistics__row">
-					<div className="stateIcon warningIcon"></div>
-					<span id="stat-toomuch" className="statistic warning">
-						You have {totalTrainers} trainers and we recommend to have maximum 25
-					</span>
-				</div>
+				{totalTrainers < 20 ? (
+					<div className="statistics__row">
+						<div className="stateIcon warningIcon"></div>
+						<span id="stat-warning" className="statistic warning">
+							You have {totalTrainers} trainers and we recommend to have at least 20
+						</span>
+					</div>
+				) : null}
+				{totalTrainers > 38 ? (
+					<div className="statistics__row">
+						<div className="stateIcon warningIcon"></div>
+						<span id="stat-toomuch" className="statistic warning">
+							You have {totalTrainers} trainers and we recommend to have maximum 25
+						</span>
+					</div>
 
-			) : null}
-			{totalTrainers >= 20 && totalTrainers <= 38 ? (
-				<div className="statistics__row">
-					<div className="stateIcon okIcon"></div>
-					<span id="stat-ok" className="statistic ok">You have {totalTrainers} trainers!</span>
-				</div>
-			) : null}
-		</>
+				) : null}
+				{totalTrainers >= 20 && totalTrainers <= 38 ? (
+					<div className="statistics__row">
+						<div className="stateIcon okIcon"></div>
+						<span id="stat-ok" className="statistic ok">You have {totalTrainers} trainers!</span>
+					</div>
+					) : null}
+			</div>
 	);
 }
 
