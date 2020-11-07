3abab1bb825d9ecb421ae9e3772697f34ed7e36c
Ivansannicolas <ivohan8@gmail.com>
2020-11-07 15:19:52 +0100

    added title to pokemon satistic

--- src/components/Deck/DeckDetails/DeckStatistics/Statistics/Statistics.jsx
+++ src/components/Deck/DeckDetails/DeckStatistics/Statistics/Statistics.jsx
@@ -3,30 +3,34 @@ import React from 'react';
 function Statistics({ decks }) {
 	let totalPokemon = decks[0]?.cards?.filter((card) => card.supertype === 'Pokémon').length;
 	return (
-		<>
-			{totalPokemon < 20 ? (
-				<div className="statistics__row">
-					<div className="stateIcon warningIcon"></div>
-					<span id="stat-warning" className="statistic warning">
-						You have {totalPokemon} pokemons and we recommend to have at least 20
-					</span>
+			<div className="statistics__category" id="statistic__category__pokemon">
+				<div className="statistics__category__name">
+					<h2 className="statistics__category-title">Pokemons</h2>
+					<div className="statistics__category-icon"></div>
 				</div>
-			) : null}
-			{totalPokemon > 38 ? (
-				<div className="statistics__row">
-					<div className="stateIcon warningIcon"></div>
-					<span id="stat-toomuch" className="statistic warning">
-						You have {totalPokemon} pokemons and we recommend to have maximum 38
-					</span>
-				</div>
-			) : null}
-			{totalPokemon >= 20 && totalPokemon <= 38 ? (
-				<div className="statistics__row">
-					<div className="stateIcon okIcon"></div>
-					<span id="stat-ok" className="statistic ok">You have {totalPokemon} pokemons!</span>
-				</div>
-			) : null}
-		</>
+				{totalPokemon < 20 ? (
+					<div className="statistics__row">
+						<div className="stateIcon warningIcon"></div>
+						<span id="stat-warning" className="statistic warning">
+							You have {totalPokemon} pokemons and we recommend to have at least 20
+						</span>
+					</div>
+				) : null}
+				{totalPokemon > 38 ? (
+					<div className="statistics__row">
+						<div className="stateIcon warningIcon"></div>
+						<span id="stat-toomuch" className="statistic warning">
+							You have {totalPokemon} pokemons and we recommend to have maximum 38
+						</span>
+					</div>
+				) : null}
+				{totalPokemon >= 20 && totalPokemon <= 38 ? (
+					<div className="statistics__row">
+						<div className="stateIcon okIcon"></div>
+						<span id="stat-ok" className="statistic ok">You have {totalPokemon} pokemons!</span>
+					</div>
+					) : null}
+			</div>
 	);
 }
 
