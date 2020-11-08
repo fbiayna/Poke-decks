3a1265398c06a57ad03e27fcba3e28368a36eeda
Ivansannicolas <ivohan8@gmail.com>
2020-11-08 04:09:29 +0100

    login page created

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import authStore from '../../stores/auth-store';
 import DeckDetails from './DeckDetails/DeckDetails';
 import DeckCards from './DeckCards/DeckCards';
 import './Deck.css'
+import Login from '../Login/Login';
 
 function Decks() {
 
-    return (
-        <div className="deck">
+    const [user, setUser] = useState(authStore.getUser());
+
+    function handleChange() {
+        setUser(authStore.getUser());
+    }
+
+    useEffect(() => {
+        authStore.addEventListener(handleChange);
+
+        return () => authStore.removeEventListener(handleChange);
+    });
+
+    return (user ? (
+        <div div className="deck" >
             <DeckDetails />
             <DeckCards />
         </div>
-    )
+    ) : (
+            <div div className="deck" >
+                <Login />
+            </div>
+        )
+    );
 }
 
 export default Decks;
