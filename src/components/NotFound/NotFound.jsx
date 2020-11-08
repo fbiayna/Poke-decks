4144452038df378a8b4837ba11307f32263faf91
fbiayna <ferranbiaynacosta@gmail.com>
2020-11-08 02:02:43 +0100

    Not Found DONE

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './NotFound.css'
+
+function NotFound() {
+
+    function goBack() {
+		window.history.back();
+    }
+    
+	return (
+		<>
+			<div className="landing-not-found">
+				<div className="landing-not-found__background"></div>
+				<div className="landing-not-found__container">
+					<h1>404 - Page Not Found! </h1>
+					<div className="container__image">
+						<img id="container__image-img"
+							alt="Not Found"
+							src="https://i.pinimg.com/originals/00/2d/57/002d5714c44f88a16c1f0bdfa97ca05e.jpg"
+						></img>
+					</div>
+                    <div className="image__button-add">
+							<button id="button-back" onClick={() => goBack()}>
+								<span class="material-icons">arrow_back</span>&nbsp;
+								<span>Go Back</span>
+							</button>
+						</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default NotFound;
