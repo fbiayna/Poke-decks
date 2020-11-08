6d96298739aeef6ed6886687bb9f77e25b55830b
Ivansannicolas <ivohan8@gmail.com>
2020-11-08 12:56:55 +0100

    login gif styles fixed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,14 +4,11 @@ import './Login.css';
 function Login() {
 
     return (
-        <section className="login">
-            <div className="login__block">
                 <div className="login__block__zoom-block">
                     <h1>Login to see your deck!</h1>
-                    <div className="login__block__img"></div>
+                    <img src="https://trello-attachments.s3.amazonaws.com/5f8ca3639574d3550b3ad495/5f9cb0e6b6fed24123256da4/6f779745f7be1f7ccd6f9547f97f851b/oie_811461cQfve2NG.gif"
+                        alt="pokemon-gif" />
                 </div>
-            </div>
-        </section>
     );
 }
 
